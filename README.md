9aef28c34bf532af0985adcac000800eb986ba4a
theblu3j <theblu3j@gmail.com>
2025-03-19 23:45:23 -0500

    VERSION 2 --- WAY BETTER YAY

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-This (very rudimentary) script autodetects your default microphone and an app of your choosing's output into a virtual microphone. This allows you to both talk and play music in games through your microphone.
+This (very rudimentary) script autodetects your default microphone and an app of your choosing's output into a virtual microphone. This allows you to both talk and play music in games or voice calls through your microphone.
 
 Limitations:
-1. Chromium/Electron apps are really weird with PipeWire and all look the same in the output list. This includes Chrome/Chromium, Discord, etc. However, PipeWire auto removes any apps from output list if they are not actively trying to play audio, so as long as you have the one you want to route currently playing audio and none others it should be easy.
-2. That last thing also means it cannot route audio into your app at the beginning without it actively playing audio. I could probably make it loop and keep trying until it succeeds in finding your app of choice, but I am a complete amateur.
-3. There is no TUI yet. To select which app you want to route into the virtual microphone, you have to use 'pw-link -o', find your app's outputs, and put them into the script. Finding default mic is pretty easy so that's already done.
-4. Some apps really don't like PipeWire being restarted when the script is ended. Either I will keep the virtual mic open even when the script is closed, or keep it how it is. I restart PipeWire just to reset settings to default.
-5. Again, this script is very crude.
+1. Chromium/Electron apps are really weird with PipeWire and all look the same in the output list.
+2. Pipewire will only list apps that are CURRENTLY playing audio. It would probably be possible to make it loop and collect multiple apps to pick from over a 5 second span, but this script was designed to be fast and easy.
--- combinedmic.sh
+++ combinedmic.sh
@@ -1,48 +1,89 @@
 #!/bin/bash
 
+# NOW PRESENTING
+# A SUPER OVER-COMPLICATED SCRIPT THAT COULD PROBABLY BE MADE SIMPLER
+# VERSION 2
 
-# find what you want to link with "pw-link -o"
-# it only finds things that are currently playing audio
-#This script operates under the distinction that the app and the mic are both two channel. If it is a mono mic or a mono output, you can probably just set the mono mic to both FL and FR without problems... probably."
-app_FL=fooyin:output_FL
-app_FR=fooyin:output_FR
+# if it wasnt fucking obvious i code like someone who's teaching themselves off the finest web searches
+# requirements: bash gum pipewire pipewire-pulse
+# WORLD OF COLOR
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
 
-# "pw-link -o" for here too
-#mic_FL=alsa_input.usb-FIFINE_Microphones_FIFINE_K780A_Microphone_REV1.0-00.analog-stereo:capture_FL
-#mic_FR=alsa_input.usb-FIFINE_Microphones_FIFINE_K780A_Microphone_REV1.0-00.analog-stereo:capture_FR
+center() {
+  termwidth="$(tput cols)"
+  padding="$(printf '%0.1s' -{1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+disconnect() {
+pw-link -d combined-sink:monitor_FL virtualmic:input_FL
+pw-link -d combined-sink:monitor_FR virtualmic:input_FR
+
+pw-link -d $mic_FL  combined-sink:playback_FL
+pw-link -d $mic_FR  combined-sink:playback_FR
+
+pw-link -d $app_FL combined-sink:playback_FL
+pw-link -d $app_FR combined-sink:playback_FR
+
+pactl unload-module module-null-sink
+printf "\n"
+exit
+}
+
+# app selection ||| also assuming you have apps and speakers which arent mono. if you do, that is unfortunate
+printf ${CYAN}
+center "Which app would you like to go into the virtual microphone?"
+tmpout=$(mktemp)
+trap "rm -f $tmpout" EXIT
+pw-link -o | grep FL | sed 's/...$//' | gum choose --height=15 | cat > $tmpout
+app="$(cat $tmpout)"
+app_FL="${app}_FL"
+app_FR="${app}_FR"
+
+# mic selection
+echo -e "Default microphone is:${LIGHTRED} $(pactl get-default-source) ${NOCOLOR}"
 mic_FL="$(pactl get-default-source):capture_FL"
 mic_FR="$(pactl get-default-source):capture_FL"
-if [[ `pw-link -o | grep $mic_FL ` ]]; then echo "mic_FL autodetected from default mic"; fi
-if [[ `pw-link -o | grep $mic_FR ` ]]; then echo "mic_FR autodetected from default mic"; fi
-echo "Default is $(pactl get-default-source)"
 
 # these create the combined sink and the virtual mic
-pactl load-module module-null-sink media.class=Audio/Sink sink_name=my-combined-sink channel_map=stereo
-pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=my-virtualmic channel_map=front-left,front-right
+# dunno why yet but the media.class bit is required
+pactl load-module module-null-sink media.class=Audio/Sink sink_name=combined-sink channel_map=stereo > /dev/null
+pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=virtualmic channel_map=front-left,front-right > /dev/null
 
-echo "Program you're trying to link to the sink usually has to be playing audio to link at the beginning. If this says failed to link ports, that is probably why"
-pw-link $app_FL my-combined-sink:playback_FL
-pw-link $app_FR my-combined-sink:playback_FR
+# link up the app FL/FR and the mic FL/FR to the combined sink
+pw-link $app_FL combined-sink:playback_FL
+pw-link $app_FR combined-sink:playback_FR
 
-pw-link $mic_FL  my-combined-sink:playback_FL
-pw-link $mic_FR  my-combined-sink:playback_FR
+pw-link $mic_FL  combined-sink:playback_FL
+pw-link $mic_FR  combined-sink:playback_FR
 
 # then the combined sink gets linked to the virtual mic
-pw-link my-combined-sink:monitor_FL my-virtualmic:input_FL
-pw-link my-combined-sink:monitor_FR my-virtualmic:input_FR
+pw-link combined-sink:monitor_FL virtualmic:input_FL
+pw-link combined-sink:monitor_FR virtualmic:input_FR
 
 echo "Combined microphone created."
 echo "Press any key to stop the combined microphone..."
-echo "Stopping usually breaks Discord and some other programs, for Discord specifically just restart it."
 
-# if user presses anything, we restart pipewire so settings get reset
 read -n 1
-systemctl --user restart pipewire
+disconnect
 
-# Notes
-# Vesktop and th-ch's Youtube Music App both use Chromium/Electron which makes it difficult/impossible to differentiate them in "pw-link -o" because both are listed as Chromium. This problem only becomes worse if you are also using a Chromium based browser such as Chrome, Vivaldi, and others. No clue if there is a fix. Solution I suggest if you are trying to play music from a Chromium/Electron based app, just don't.
-# Another thing to keep in mind is that Discord's input sensitivity will take effect on your virtual microphone, causing it to blink in and out. Slightly annoying, so I recommend using this in game only unless you have fancy schmancy voice activity that won't kill the audio.
-
-# this is very rudimentary code, mostly copy pasting from other various websites and throwing it in a blender and running it and praying.
-
-# in the future I might update this script to be more interactive and user friendly. would probably have to run pw-link -o, print it, ask the user which ones to link. probably wouldn't be too hard. probably.
+# LIMITATIONS
+# STILL NO SUPPORT FOR MONO MICROPHONES
+# IF SOMEBODY OTHER THAN ME EVER USES THIS PIECE OF SHIT I WILL FIX IT THEN
+# REQUIRES THE APP OUTPUT YOU CHOOSE TO BE CURRENTLY PLAYING AUDIO
+# EXCEPT FOR DISCORD BECAUSE DISCORD IS SPECIAL AND IS SOMEHOW CONSTANTLY PLAYING FUCKING AUDIO AND IS ALWAYS LISTED AS CHROMIUM WHICH MAKES IT A FUCKING PAIN TO PICK ANY OTHER CHROMIUM OUTPUT BECAUSE THEY LOOK THE SAME
